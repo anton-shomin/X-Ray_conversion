8f4ad1368af4d6489d7886212b10a97b73e9a452
Anton Shomin <anton_shomin@epam.com>
2024-07-11 21:55:11 +0300

    common solution runner added

--- x-ray-solution.py
+++ x-ray-solution.py
@@ -1,2 +1,6 @@
 import excel_to_xml_converter
 import xml_pretifier
+
+excel_to_xml_converter.main(
+    "/Users/antonshomin/Projects/X-RAY challenge/check-tests")
+xml_pretifier.main("/Users/antonshomin/Projects/X-RAY challenge/check-tests")
