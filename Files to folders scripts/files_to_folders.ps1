e30a046eb72514873e362640d1bc57bd1f5822dc
Anton Shomin <anton_shomin@epam.com>
2024-07-22 13:23:52 +0300

    Files to folders scripts added for win, mac/linux

---
+++ Files to folders scripts/files_to_folders.ps1
@@ -0,0 +1,19 @@
+# Get directory where the script is located
+$dirPath = Get-Location
+
+# Change directory to dirPath - in PowerShell this isn't necessary as all paths are absolute.
+
+# Get all items in the directory
+$items = Get-ChildItem -Path $dirPath -File
+
+# Loop through each item in the directory
+foreach($item in $items) {
+    # Get file name without extension
+    $filename = [IO.Path]::GetFileNameWithoutExtension($item.Name)
+
+    # Create new directory with filename, -Force flag is used to avoid error if directory already exists
+    New-Item -Path $dirPath -Name $filename -ItemType Directory -Force | Out-Null
+
+    # Move file to the new directory
+    Move-Item -Path $item.FullName -Destination (Join-Path $dirPath $filename)
+}
---
+++ Files to folders scripts/files_to_folders.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# get directory where the script is located
+dir_path="$PWD"
+
+# change directory to dir_path
+cd "$dir_path"
+
+# loop through all files in the directory
+for file in *; do
+    # skip directories
+    if [[ -d $file ]]; then continue; fi
+
+    # get file name without extension
+    filename="${file%.*}"
+
+    # create new directory with filename, -p flag is used to avoid error if directory already exists
+    mkdir -p "$filename"
+
+    # move file to the new directory
+    mv "$file" "$filename"
+done
